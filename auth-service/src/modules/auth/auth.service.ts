e0dd329cb66bf76bf35ccad9a77234fecc425c61
shamil <muhammedshamil470@gmail.com>
2026-02-17 07:17:15 +0530

    done regisration section

---
+++ auth-service/src/modules/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { eq } from 'drizzle-orm'
+import bycrypt from 'bcrypt'
+import { db } from '../../db'
+import { users } from '../../../drizzle/schema'
+import { logger } from '../../config/logger'
+import { RegisterRequest } from '../../types/authType'
+
+
+export class AuthService {
+    async register(registerRequest: RegisterRequest,) {
+        try {
+            const existingUser = await db.select().from(users).where(eq(users.email, registerRequest.email)).execute()
+            if (existingUser.length > 0) {
+                logger.warn({ email: registerRequest.email }, 'Attempt to register with an existing email');
+                throw new Error('Email already in use')
+            }
+            
+            const salt = await bycrypt.genSalt(10)
+            const passwordHash = await bycrypt.hash(registerRequest.password, salt)
+
+            const newUser = await db.insert(users).values({
+                email: registerRequest.email,
+                passwordHash: passwordHash,
+                role: registerRequest.role,
+            }).returning()
+
+            return newUser[0]
+        } catch (error) {
+            logger.error({ error }, 'User registration failed');
+            throw new Error('Failed to register user');
+        }
+    }
+}
