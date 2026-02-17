1403cec90c3ef3e56ce1055a4ad7a4dc64445fa5
shamil <muhammedshamil470@gmail.com>
2026-02-17 22:08:10 +0530

    create auth routes

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
-
-
+import { authRouter } from "./modules/auth/auth.routes";
+import {errorHandler} from "@cab/observability";
 
 export const app = express()
 
 
+app.use(errorHandler)
 
 app.use(express.json());
+app.use('/auth',authRouter)
+
+
+
+app.use(errorHandler)
+
