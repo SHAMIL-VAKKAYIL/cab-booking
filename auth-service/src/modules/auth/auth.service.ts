5c380eb124589e4326d9c2670b8b60614a1d84f4
shamil <muhammedshamil470@gmail.com>
2026-02-17 22:05:32 +0530

    done login service

--- auth-service/src/modules/auth/auth.service.ts
+++ auth-service/src/modules/auth/auth.service.ts
@@ -14,7 +14,6 @@ export class AuthService {
                 logger.warn({ email: registerRequest.email }, 'Attempt to register with an existing email');
                 throw new Error('Email already in use')
             }
-            
             const salt = await bycrypt.genSalt(10)
             const passwordHash = await bycrypt.hash(registerRequest.password, salt)
 
@@ -29,5 +28,28 @@ export class AuthService {
             logger.error({ error }, 'User registration failed');
             throw new Error('Failed to register user');
         }
+    };
+
+    async login(email: string, password: string) {
+        try {
+            const existingUser =await db.select().from(users).where(eq(users.email,email)).execute()
+            if (existingUser.length === 0) {
+                logger.warn({ email }, 'Attempt to login with non-existent email');
+                throw new Error('Invalid email or password')
+            }
+
+            const user = existingUser[0]
+            const isPasswordValid = await bycrypt.compare(password, user.passwordHash)
+            if (!isPasswordValid) {
+                logger.warn({ email }, 'Attempt to login with incorrect password');
+                throw new Error('Invalid email or password')
+            }
+            return user
+            
+        } catch (error) {
+            logger.error({ error }, 'User login failed');
+            throw new Error('Failed to login user');
+        }
+
     }
 }
