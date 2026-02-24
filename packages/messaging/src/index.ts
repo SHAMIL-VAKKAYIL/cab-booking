f14b1bfb7783965bfcb4bd27b6b13ba8d210d8cf
shamil <muhammedshamil470@gmail.com>
2026-02-24 07:58:06 +0530

    create package for kafka

--- packages/messaging/src/index.ts
+++ packages/messaging/src/index.ts
@@ -1,4 +1,3 @@
 export * from './kafka';
 export * from './producer';
 export * from './consumer';
-export * from './topics';
--- packages/messaging/src/topics.ts
+++
@@ -1,4 +0,0 @@
-export const Topics = {
-  USER_CREATED: 'user.created',
-  BOOKING_CREATED: 'booking.created',
-};
