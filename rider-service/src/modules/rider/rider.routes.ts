3f2a012d893feaded64b729735ef5c207e0f5b4e
muhammed shamil <muhammedshamil470@gmail.com>
2026-03-10 12:13:00 +0530

    create:rider history routes

--- rider-service/src/modules/rider/rider.routes.ts
+++ rider-service/src/modules/rider/rider.routes.ts
@@ -1,9 +1,12 @@
 import {Router} from "express";
-import { profileCreation } from "./rider.controller";
+import { getRiderProfile, profileCreation, rideHistoryController } from "./rider.controller";
 
 const riderRouter:Router = Router()
 
-riderRouter.post('/v1/update-profile',profileCreation)
+riderRouter.post('/v1/update-profile',profileCreation);
+riderRouter.get('/v1/get-profile',getRiderProfile)
+riderRouter.get('/v1/history',   rideHistoryController.getHistory)
+riderRouter.get('/v1/history/:tripId',rideHistoryController.getTripDetail)
 
 
 export { riderRouter }
