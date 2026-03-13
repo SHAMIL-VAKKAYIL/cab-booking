774e06906110c228b52e13d189b9692da63827c4
shamil <muhammedshamil470@gmail.com>
2026-03-13 22:41:02 +0530

    modify:driver schema

--- driver-service/src/db/schema.ts
+++ driver-service/src/db/schema.ts
@@ -4,12 +4,12 @@ import {
     varchar,
     boolean,
     timestamp,
-    pgEnum
+    pgEnum,
+    numeric,
+    integer
 } from "drizzle-orm/pg-core";
 
-/**
- * Driver status enum
- */
+
 export const driverStatusEnum = pgEnum("driver_status", [
     "PENDING",
     "APPROVED",
@@ -23,23 +23,29 @@ export const driverAvailabilityEnum = pgEnum("driver_availability", [
     "ON_TRIP",
 ]);
 
+export const vehicleTypeEnum = pgEnum("vehicle_type", ["ECONOMY", "PREMIUM"]);
+
 export const driver = pgTable("drivers", {
     id: uuid('id').primaryKey().defaultRandom(),
-    name: varchar('name', { length: 100 }).notNull(),
+    name: varchar('name', { length: 100 }),
     user_id: uuid('user_id').notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    phone: varchar("phone", { length: 20 }),
     //! license details
-    licenseNumber: varchar('license_number', { length: 100 }).notNull(),
-    licenseExpiry: timestamp('license_expiry', { withTimezone: true }).notNull(),
+    licenseNumber: varchar('license_number', { length: 100 }),
+    licenseExpiry: timestamp('license_expiry', { withTimezone: true }),
     //! vehicle details
-    vehicleModel: varchar('vehicle_model', { length: 50 }).notNull(),
-    vehiclePlate: varchar('vehicle_plate', { length: 50 }).notNull(),
+    vehicleModel: varchar('vehicle_model', { length: 50 }),
+    vehiclePlate: varchar('vehicle_plate', { length: 50 }),
+    vehicleType: vehicleTypeEnum("vehicle_type"),
 
 
     status: driverStatusEnum('status').default('APPROVED').notNull(),
-
-    availablity: driverAvailabilityEnum('availablity').default('OFFLINE').notNull(),
-
+    availability: driverAvailabilityEnum('availability').default('OFFLINE').notNull(),
     isActive: boolean("is_active").default(true).notNull(),
+
+    averageRating: numeric("average_rating", { precision: 3, scale: 2 }).notNull().default("0"),
+    totalRatings: integer("total_ratings").notNull().default(0),
 
     createdAt: timestamp("created_at", { withTimezone: true })
         .defaultNow()
