91788ee5a3b1bf00c9d8c6331efd6480202822ec
shamil <muhammedshamil470@gmail.com>
2026-03-19 05:19:39 +0530

    chage gateway port

--- gateway/src/config.ts
+++ gateway/src/config.ts
@@ -4,13 +4,13 @@ export const config = {
     secret: process.env.JWT_SECRET!,
   },
   services: {
-    auth:         process.env.AUTH_SERVICE_URL         || 'http://localhost:3001',
-    driver:       process.env.DRIVER_SERVICE_URL       || 'http://localhost:3002',
-    rider:        process.env.RIDER_SERVICE_URL        || 'http://localhost:3003',
-    trip:         process.env.TRIP_SERVICE_URL         || 'http://localhost:3006',
-    booking:      process.env.BOOKING_SERVICE_URL      || 'http://localhost:3005',
-    payment:      process.env.PAYMENT_SERVICE_URL      || 'http://localhost:3008',
-    pricing:      process.env.PRICING_SERVICE_URL      || 'http://localhost:3007',
-    notification: process.env.NOTIFICATION_SERVICE_URL || 'http://localhost:3009',
+    auth: process.env.AUTH_SERVICE_URL || 'http://localhost:4001',
+    driver: process.env.DRIVER_SERVICE_URL || 'http://localhost:4002',
+    rider: process.env.RIDER_SERVICE_URL || 'http://localhost:4003',
+    trip: process.env.TRIP_SERVICE_URL || 'http://localhost:4004',
+    booking: process.env.BOOKING_SERVICE_URL || 'http://localhost:4005',
+    payment: process.env.PAYMENT_SERVICE_URL || 'http://localhost:4008',
+    pricing: process.env.PRICING_SERVICE_URL || 'http://localhost:4007',
+    notification: process.env.NOTIFICATION_SERVICE_URL || 'http://localhost:4009',
   }
 }
