8e4be9128b5a080b82a62bbfc3a655e9b2666e1d
muhammed shamil <muhammedshamil470@gmail.com>
2026-03-18 15:39:26 +0530

    update:init redis , producer and consumer

--- trip-service/src/server.ts
+++ trip-service/src/server.ts
@@ -2,14 +2,28 @@ import { app } from './app';
 import { config } from './config';
 import { logger } from './config/logger';
 import { pool } from './db/pool';
+import { connectProducer } from '@cab/messaging'
+import { startRideCancelledConsumer } from './events/consumer/trip-cancelled.consumer';
+import { startTripCreateConsumer } from './events/consumer/trip-create.consumer';
+import { connectRedis } from './redis';
 
 
 const PORT = config.port;
 const start = async () => {
   try {
 
-    await pool.connect();
-    logger.info({}, 'database connected');
+    await pool.connect()
+    logger.info({}, 'Database connected')
+
+    await connectRedis()
+    logger.info({}, 'Redis connected')
+
+    await connectProducer()
+    logger.info({}, 'Kafka producer connected')
+
+    await startTripCreateConsumer()
+    await startRideCancelledConsumer()
+    logger.info({}, 'Kafka consumers started')
 
     app.listen(PORT, () => {
       logger.info({}, `Server is running on port ${PORT}`);
