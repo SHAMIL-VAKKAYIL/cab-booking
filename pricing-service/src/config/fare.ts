1c58f4bf1a30f37a0c68293adfd2961bac5f9dec
muhammed shamil <muhammedshamil470@gmail.com>
2026-03-23 16:58:18 +0530

    configure fare and logger and create server

---
+++ pricing-service/src/config/fare.ts
@@ -0,0 +1,15 @@
+export const FARE_CONFIG = {
+    ECONOMY: {
+        baseFare: 60,
+        pricePerKm: 12,
+        pricePerMin: 1.5,
+        vehicleMultiplier: 1.0,
+    },
+    PREMIUM: {
+        baseFare: 120,
+        pricePerKm: 20,
+        pricePerMin: 2.5,
+        vehicleMultiplier: 1.5,
+    },
+    avgSpeedKmh: 50,
+}
