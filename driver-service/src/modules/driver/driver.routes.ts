e44875c4b22513e638578a0fc8f569be0a6ec47b
shamil <muhammedshamil470@gmail.com>
2026-03-25 20:58:20 +0530

    complete driver routes

--- driver-service/src/modules/driver/driver.routes.ts
+++ driver-service/src/modules/driver/driver.routes.ts
@@ -1,9 +1,18 @@
-import {Router} from "express";
-import { profileCreation } from "./driver.controller";
+import { Router } from 'express'
+import {
+  getProfile,
+  updateProfile,
+  updateVehicle,
+  toggleAvailability,
+  getRating,
+} from './driver.controller'
 
 const driverRouter:Router = Router()
 
-driverRouter.post('/update-profile',profileCreation)
-
+driverRouter.get('/v1/get-profile',          getProfile)
+driverRouter.post('/v1/update-profile',       updateProfile)
+driverRouter.post('/v1/vehicle',              updateVehicle)
+driverRouter.post('/v1/toggle-availability',  toggleAvailability)
+driverRouter.get('/v1/rating',               getRating)
 
 export { driverRouter }
