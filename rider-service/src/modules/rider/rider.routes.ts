542353babcf79a78dd585ac1c0ff51b39df41430
shamil <muhammedshamil470@gmail.com>
2026-03-27 23:02:44 +0530

    modify: routes

--- rider-service/src/modules/rider/rider.routes.ts
+++ rider-service/src/modules/rider/rider.routes.ts
@@ -2,23 +2,24 @@ import { Router } from 'express';
 import {
   cancelRide,
   deleteSavedPlace,
+  getHistory,
   getMyRating,
   getRiderProfile,
   getSavedPlaces,
+  getTripDetail,
   profileCreation,
   rateDriver,
   requestRide,
-  rideHistoryController,
   savePlace,
 } from './rider.controller';
 
 const riderRouter: Router = Router();
 
-riderRouter.post('/v1/update-profile', profileCreation);
+riderRouter.put('/v1/update-profile', profileCreation);
 riderRouter.get('/v1/get-profile', getRiderProfile);
 
-riderRouter.get('/v1/history', rideHistoryController.getHistory);
-riderRouter.get('/v1/history/:tripId', rideHistoryController.getTripDetail);
+riderRouter.get('/v1/history', getHistory);
+riderRouter.get('/v1/history/:tripId', getTripDetail);
 
 riderRouter.post('/v1/ratings', rateDriver);
 riderRouter.get('/v1/ratings/me', getMyRating);
@@ -27,6 +28,6 @@ riderRouter.post('/v1/saved-places', savePlace);
 riderRouter.get('/v1/saved-places', getSavedPlaces);
 riderRouter.delete('/v1/saved-places/:placeId', deleteSavedPlace);
 
-riderRouter.post('/v1/rides/request', requestRide)
-riderRouter.delete('/v1/rides/:rideId/cancel', cancelRide)
+riderRouter.post('/v1/rides/request', requestRide);
+riderRouter.delete('/v1/rides/:rideId/cancel', cancelRide);
 export { riderRouter };
