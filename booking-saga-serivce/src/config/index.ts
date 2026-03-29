cb07f8e07c132ddad686000b7632b28cb71f31bc
shamil <muhammedshamil470@gmail.com>
2026-03-29 21:52:56 +0530

    configure enviroment variables and logger

---
+++ booking-saga-serivce/src/config/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+
+export const config = {
+    port:parseInt(process.env.PORT || '4005',10),
+    nodeEnv:process.env.NODE_ENV || 'development',
+    db:{
+    host:process.env.DB_HOST!,
+    database:process.env.DB_NAME!,
+    user:process.env.DB_USER!,
+    password:process.env.DB_PASSWORD!,
+    port:Number(process.env.DB_PORT),
+    }
+};
