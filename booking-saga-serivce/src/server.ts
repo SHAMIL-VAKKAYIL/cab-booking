3c1da271a436cf3b0945ecef062c9c9284184f88
shamil <muhammedshamil470@gmail.com>
2026-04-02 07:03:29 +0530

    init db pool ,consumer connection and producer

--- booking-saga-serivce/src/server.ts
+++ booking-saga-serivce/src/server.ts
@@ -1,19 +1,37 @@
 import { app } from "./app";
 import { logger } from "./config/logger";
 import { config } from "./config";
+import { pool } from "./db/pool";
+import { connectProducer } from "@cab/messaging";
+import { startRideRequestedConsumer } from "./events/consumers/ride-requested.consumer";
+import { startFareReplyConsumer } from "./events/consumers/fare-reply.consumer";
+import { startDriverReplyConsumer } from "./events/consumers/driver-reply.consumer";
+import { startTripReplyConsumer } from "./events/consumers/trip-reply.consumer";
+import { startRideCancelledConsumer } from "./events/consumers/ride-cancelled.consumer";
 
-const start =async()=>{
-    try {
-    
+const start = async () => {
+  try {
+    await pool.connect();
+    logger.info("Database connected");
 
-    app.listen(config.port,()=>{
-        logger.info(`Booking saga service running on port ${config.port}`)
-    })
-        
-    } catch (error) {
-        logger.error({error},'Failed to start booking saga service')
-        process.exit(1)
-    }
-}
-start()
+    await connectProducer();
+    logger.info("Kafka producer connected");
 
+    await startRideRequestedConsumer();
+    await startFareReplyConsumer();
+    await startDriverReplyConsumer();
+    await startTripReplyConsumer();
+    await startRideCancelledConsumer();
+
+    logger.info("All Kafka consumers started");
+
+    app.listen(config.port, () => {
+      logger.info(`Booking saga service running on port ${config.port}`);
+    });
+
+  } catch (error) {
+    logger.error({ error }, "Failed to start booking saga service");
+    process.exit(1);
+  }
+};
+start();
