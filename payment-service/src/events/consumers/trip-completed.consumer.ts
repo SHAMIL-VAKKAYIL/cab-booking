50fb6af9c002f41ae9006c993ffdd9f130966dbc
shamil <muhammedshamil470@gmail.com>
2026-04-04 09:53:45 +0530

    create: payment completed consumer

---
+++ payment-service/src/events/consumers/trip-completed.consumer.ts
@@ -0,0 +1,37 @@
+import { Topics } from '@cab/events'
+import { createConsumer } from '@cab/messaging'
+import { PaymentService } from '../../modules/payments/payment.service'
+import { logger } from '../../config/logger'
+
+const paymentService = new PaymentService()
+
+
+export const startTripCompletedConsumer = async () => {
+    await createConsumer({
+        groupId: 'payment-service-trip-completed',
+        topic: Topics.TRIP_COMPLETED,
+        fromBeginning: false,
+        retries: 3,
+        dlqTopic: 'trip.completed.dlq',
+        retryDelayMs: 1000,
+        eachMessage: async ({ key, value }) => {
+            logger.info({ key, value }, "Received TRIP_COMPLETED event");
+            if (!value) return;
+            const { data } = value
+            const { tripId, driverId, riderId, amount } = data
+            if (!tripId || !riderId || !driverId || !amount) {
+                logger.error({ data }, 'trip.completed missing required fields, skipping')
+                return
+            }
+
+            await paymentService.proccessPayment({
+                tripId,
+                riderId,
+                driverId,
+                amount: Number(amount),
+            })
+            logger.info({ tripId }, 'Payment processed from trip.completed')
+        }
+
+    })
+}
