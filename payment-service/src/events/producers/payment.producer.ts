d14b8cd1eb55c189277e2fff5a5ec08b5d173086
shamil <muhammedshamil470@gmail.com>
2026-04-04 09:52:34 +0530

    create: payment producers

---
+++ payment-service/src/events/producers/payment.producer.ts
@@ -0,0 +1,50 @@
+import { publishEvent } from "@cab/messaging";
+import { Topics, PaymentFailedEvent, PaymentSuccessEvent } from "@cab/events";
+import { randomUUID } from 'crypto'
+
+
+export const publishPaymentSuccess = async (payload: {
+    paymentId: string
+    tripId: string
+    riderId: string
+    driverId: string
+    amount: number
+    currency: string
+    transactionId: string
+}) => {
+    const event: PaymentSuccessEvent = {
+        event: 'PAYMENT_SUCCESS',
+        data: {
+            ...payload,
+            occurredAt: new Date().toISOString()
+        },
+        metadata: {
+            correlationId: randomUUID(),
+            source: 'payment-service',
+            version: 1
+        }
+    }
+    await publishEvent(Topics.PAYMENT_SUCCESS, event);
+}
+
+export const publishPaymentFailed = async (payload: {
+    paymentId: string
+    tripId: string
+    riderId: string
+    reason: string
+}) => {
+    const event: PaymentFailedEvent = {
+        event: 'PAYMENT_FAILED',
+        data: {
+            ...payload,
+            occurredAt: new Date().toISOString()
+        },
+        metadata: {
+            correlationId: randomUUID(),
+            source: 'payment-service',
+            version: 1
+        }
+    }
+
+    await publishEvent(Topics.PAYMENT_FAILED, event);
+}
