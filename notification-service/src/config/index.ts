7493427c3624dbd6cd0739724b47fbf2db3f4aa6
shamil <muhammedshamil470@gmail.com>
2026-04-05 10:51:38 +0530

    configure nodemailer

--- notification-service/src/config/index.ts
+++ notification-service/src/config/index.ts
@@ -4,4 +4,8 @@ dotenv.config();
 export const config = {
   port: parseInt(process.env.PORT || "4008", 10),
   nodeEnv: process.env.NODE_ENV || "development",
+  mailer: {
+    email: process.env.GMAIL_USER,
+    password: process.env.GMAIL_APP_PASSWORD,
+  },
 };
---
+++ notification-service/src/config/mailer.ts
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+import { logger } from "./logger";
+import { config } from ".";
+
+if (!process.env.GMAIL_USER || !process.env.GMAIL_APP_PASSWORD) {
+  throw new Error("GMAIL_USER and GMAIL_APP_PASSWORD must be set");
+}
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: config.mailer.email,
+    pass: config.mailer.password,
+  },
+});
+
+export const verifyMailer = async () => {
+  try {
+    await transporter.verify();
+    logger.info("Mailer is working");
+  } catch (error) {
+    logger.error({ error }, "Mailer is not working");
+  }
+};
