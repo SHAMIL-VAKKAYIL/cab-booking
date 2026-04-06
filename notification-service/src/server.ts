e126950a5d37f318383271ef710f2eb6f77b7734
shamil <muhammedshamil470@gmail.com>
2026-04-06 22:35:54 +0530

    init: all consumers

--- notification-service/src/server.ts
+++ notification-service/src/server.ts
@@ -1,12 +1,34 @@
 import { app } from "./app";
 import { config } from "./config";
 import { logger } from "./config/logger";
+import { verifyMailer } from "./config/mailer";
+import { connectProducer } from "@cab/messaging";
+import { startBookingConfirmedConsumer } from "./events/consumers/booking-confirmed.consumer";
+import { startBookingFailedConsumer } from "./events/consumers/booking-failed.consumer";
+import { startTripStartedConsumer } from "./events/consumers/trip-started.consumer";
+import { startTripCompletedConsumer } from "./events/consumers/trip-completed.consumer";
+import { startTripCancelledConsumer } from "./events/consumers/trip-cancelled.consumer";
+import { startPaymentSuccessConsumer } from "./events/consumers/payment-success.consumer";
+import { startPaymentFailedConsumer } from "./events/consumers/payment-failed.consumer";
+
 
 const PORT = config.port;
 const start = async () => {
   try {
+    await Promise.all([
+      verifyMailer(),
+      connectProducer(),
+      startBookingConfirmedConsumer(),
+      startBookingFailedConsumer(),
+      startTripStartedConsumer(),
+      startTripCompletedConsumer(),
+      startTripCancelledConsumer(),
+      startPaymentSuccessConsumer(),
+      startPaymentFailedConsumer(),
+    ]);
+    logger.info("All Kafka consumers,mailer and producer started'");
     app.listen(PORT, () => {
-    logger.info(`server runnig on ${PORT}` )
+      logger.info(`server runnig on ${PORT}`);
     });
   } catch (error) {
     logger.error({ error }, "faild to start server");
