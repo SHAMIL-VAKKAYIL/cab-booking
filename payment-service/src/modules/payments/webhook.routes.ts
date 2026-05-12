bfc06e4ca1edb38f9699bd5c187b34aaece2aa28
muhammed shamil <muhammedshamil470@gmail.com>
2026-05-12 12:25:55 +0530

    create payment order getby id for user

--- payment-service/src/modules/payments/webhook.routes.ts
+++ payment-service/src/modules/payments/webhook.routes.ts
@@ -4,5 +4,6 @@ import { handleRazorpay } from './webhook.controller';
 const paymentRouter = Router()
 
 paymentRouter.post('/v1/webhook/razorpay', express.raw({ type: 'application/json' }), handleRazorpay);
+paymentRouter.get('/trip/:tripId', getPaymentByTrip);
 
 export default paymentRouter;
