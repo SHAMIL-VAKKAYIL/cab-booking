5cca49ab38c75d96f7c0a6eee753fafd73394e89
shamil <muhammedshamil470@gmail.com>
2026-05-22 10:09:09 +0530

    create: add prometheus router

--- notification-service/src/app.ts
+++ notification-service/src/app.ts
@@ -1,4 +1,4 @@
-import { errorHandler } from "@cab/observability";
+import { errorHandler, metricsMiddleware, registry } from "@cab/observability";
 import express from "express";
 import { logger } from "./config/logger";
 
@@ -10,6 +10,13 @@ app.use((req, res, next) => {
   logger.info({ url: req.url }, "request received");
   next();
 });
+
+app.use(metricsMiddleware)
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', registry.contentType)
+  res.send(await registry.metrics())
+})
 
 app.use('/health', (req, res) => {
   res.send('OK');
